52e731fb27ac36196f04af2a9dda41e435c8eb37
mokuzaru <kenny.otoya@pucp.pe>
2017-06-01 02:09:32 -0500

    first

---
+++ motores_driver_test/motores_driver_test.ino
@@ -0,0 +1,20 @@
+N1=6;
+N2=9;
+N3=10;
+N4=11;
+void setup() {
+  pinMode(N1, OUTPUT);
+  pinMode(N2, OUTPUT);
+  pinMode(N3, OUTPUT);
+  pinMode(N4, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(N1,LOW);
+  digitalWrite(N2,LOW);
+  digitalWrite(N3,LOW);
+  digitalWrite(N4,LOW);
+  delay(1000);
+  
+
+}
